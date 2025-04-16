15886cf1087d89b0f3b8aaf5b350d33078ae5262
taruna-webexp <taruna.webexpert@gmail.com>
2025-04-16 16:52:24 +0530

    added twiiter-client with bot

---
+++ agent/client-twitter/src/index.ts
@@ -0,0 +1,8 @@
+import { TwitterClientInterface } from "./client";
+
+const twitterPlugin = {
+    name: "twitter",
+    description: "Twitter client",
+    clients: [TwitterClientInterface],
+};
+export default twitterPlugin;
